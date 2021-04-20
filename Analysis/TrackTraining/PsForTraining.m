8adc0605a0c07e93176145cc096ab873f9489352
Alice Dallstream <alicedallst@gmail.com>
2021-04-20 12:14:12 -0400

    Update PsForTraining.m

--- Analysis/TrackTraining/PsForTraining.m
+++ Analysis/TrackTraining/PsForTraining.m
@@ -1,4 +1,11 @@
 % Generate percentages for P1:9 for training
-sd = 1.5; %adjust this as needed
+sd = 2; %adjust this as needed
 Ps = 2*normpdf([0.5:8.5],0,sd) %use this output
 sumPs = sum(2*normpdf([0.5:8.5],0,sd)) %check it adds up
+
+%Estimate trials for 30 minutes
+oneside = normpdf([0.5:8.5],0,sd);
+%Both sides evenly
+(oneside+fliplr(oneside)).*300
+%Subset in even for oneside
+oneside.*300
