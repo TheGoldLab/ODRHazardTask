99d0cf8ab245cda6324ce179e9b3757cfe7c2638
KyraSchaps <45470033+KyraSchaps@users.noreply.github.com>
2021-08-19 11:57:59 -0400

    Update KAS_Parser_794_DelayNeural.m

--- Analysis/KyraPipeline20210129/KAS_Parser_794_DelayNeural.m
+++ Analysis/KyraPipeline20210129/KAS_Parser_794_DelayNeural.m
@@ -1,7 +1,7 @@
 function [ allData ] = KAS_Parser_794_DelayNeural( Cutoff, Type, data, theFile )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-%test
+
 %%Code to analyize the bias in responses based on SACP, current target, and
 %sample mean
 
@@ -11,7 +11,7 @@ function [ allData ] = KAS_Parser_794_DelayNeural( Cutoff, Type, data, theFile )
 %decision period
 
 %Gather Data
-data_path = '/Users/KAS/Documents/MATLAB/ODR Task Analysis/AdaptODRMonkey/DatatoParse2';%AVData/794'; % DatatoParse2';
+data_path = 'C:\Users\kyras\Desktop\UPenn 17-18\Matlab_AODR\ODRHazardTask-main\ConvertedFiles';%AVData/794'; % DatatoParse2';
 % files = dir(fullfile(data_path, '*.mat'));
 % nfiles = length(files);
 cd(data_path);
@@ -21,7 +21,7 @@ cd(data_path);
 %  1. session
 %  2. Trial index within session
 %  3. Hazard rate
-%  4. Noise
+%  4. Noise (not currently used)
 %  5. TACP(Actual)
 %  6. T1_Angle
 %  7. T2_Angle
@@ -34,8 +34,15 @@ cd(data_path);
 %  14. Choice
 %  15. Choice~=Correct(previous trial) aka switch
 %  16. LLR for Change point
-
-
+%  17. Online score
+%  18. Correct 
+%  19. Sample On time
+%  20. Sample Off time
+%  21. FP Off time
+%  22. Sac On Time
+%  23. Targ Acq Time
+%  24. Number of units this session
+%  25-end, spike times for units this session
 
 
 %%
@@ -48,35 +55,7 @@ spikies=[];
 % Loop through the files
 nfiles=1;
 for currentFile = 1:nfiles
-   
-   % load the data file
-%    load(fullfile(data_path, theFile));
-%    disp(files(currentFile).name);
-%       
 
-   
-%      ActiveTarg=data.ecodes.data(:,35);
-%    InactiveTarg=nans(size(data.ecodes.data(:,41)));
-%    InactiveTarg(data.ecodes.data(:,35)==data.ecodes.data(:,36))=data.ecodes.data(data.ecodes.data(:,35)==data.ecodes.data(:,36),37);
-%    InactiveTarg(data.ecodes.data(:,35)~=data.ecodes.data(:,36))=data.ecodes.data(data.ecodes.data(:,35)~=data.ecodes.data(:,36),36);
-% 
-%    
-%    Choice=nans(size(data.ecodes.data(:,41)));
-% 
-%   RespAng=data.ecodes.data(:, [39]);
-% 
-% 
-% for i=1:size(data.ecodes.data(:,41),1)
-%     if ~isnan(ActiveTarg(i))
-%     if abs(degAngDiff(ActiveTarg(i),RespAng(i)))<=Cutoff
-%         Choice(i)=ActiveTarg(i);
-%     elseif abs(degAngDiff(InactiveTarg(i),RespAng(i)))<=Cutoff
-%             Choice(i)=InactiveTarg(i);
-% 
-%     end
-%     end
-% end
-%    
    % select "good" trials (no broken fixation) (Online score is not BF) and
    % is a AdODR task (taskid>=2);
    if Type==1
@@ -89,14 +68,11 @@ for currentFile = 1:nfiles
    
    
       %Which target was picked?
-%    Choice=nans(size(data.ecodes.data(:,41)));
-%    Choice(data.ecodes.data(:,41)==1)=data.ecodes.data(data.ecodes.data(:,41)==1,35);
+
    InactiveTarg=nan(size(data.ecodes.data(:,41)));
    InactiveTarg(data.ecodes.data(:,35)==data.ecodes.data(:,36))=data.ecodes.data(data.ecodes.data(:,35)==data.ecodes.data(:,36),37);
    InactiveTarg(data.ecodes.data(:,35)~=data.ecodes.data(:,36))=data.ecodes.data(data.ecodes.data(:,35)~=data.ecodes.data(:,36),36);
 
-%    Choice(data.ecodes.data(:,41)==0)=ChoiceParser(data.ecodes.data(:,41)==0);
-%    Choice=Choice(UnbrokenTrials);
 
 Choice2=nan(size(data.ecodes.data(UnbrokenTrials,41)));
   ActiveTarg=data.ecodes.data(UnbrokenTrials,35);
@@ -156,9 +132,6 @@ end
    %Were the choices visbible?
    Choice_Vis=~isnan(data.ecodes.data(UnbrokenTrials,8)) & (data.ecodes.data(UnbrokenTrials,8))<(data.ecodes.data(UnbrokenTrials,9));
    
-
-
-
   %Was the choice equal to the correct target of the previous trial?
   ActiveTarg=data.ecodes.data(UnbrokenTrials,35);
  
@@ -194,26 +167,7 @@ end
         %Allows you to assign a matrix to the slots of a matrix
    end
 
-%    
-%    has_target=~isnan(data.ecodes.data(UnbrokenTrials,6));
-%    has_mean=~isnan(data.ecodes.data(UnbrokenTrials,22));
-%    mean_visible=data.ecodes.data(UnbrokenTrials,32)<90;
-%    
-% 
-%    
-%    %Previous Target difference
-%    PTargTargDiff=degAngDiff(tce_angles(2:end,2),tce_angles(1:end-1,2));
-%    PTargTargDiff=[0;PTargTargDiff];
-%    
-%    %Delay Time
-%    Delay=data.ecodes.data(:,8)-data.ecodes.data(:,7);
-%    
-%    %RXNTime
-%    RXNTime=data.ecodes.data(UnbrokenTrials,42);
-% 
-%    %Model based estimate of where the mean is
-%    PredictedMeans=CPPandRelia(tce_angles(:,2)', .1, 25 )';
-   
+
 imptUnits=(mod(data.spikes.id,10)~=0);
 %imptUnits=ones(size(data.spikes.id));
 numNeurons=sum(imptUnits);
@@ -235,21 +189,8 @@ numNeurons=sum(imptUnits);
       LLRCP,...
       data.ecodes.data(UnbrokenTrials,[41]),...
       Choice2==data.ecodes.data(UnbrokenTrials, [35]),...
-      data.ecodes.data(UnbrokenTrials, [7 9 10 12]),...
+      data.ecodes.data(UnbrokenTrials, [7 9 10 12,13]),...
       numNeurons.*ones(sum(UnbrokenTrials), 1)
-      %*****
-%       data.spikies.data(UnbrokenTrials,2)
-%       t12_angles, ...
-%       has_target,...
-%       has_mean,...
-%       data.ecodes.data(UnbrokenTrials, [49 42 38 40, 41, 37]), ... 
-%       PTargTargDiff,...
-%       nans(sum(UnbrokenTrials),2),...
-%       RXNTime,...
-%       Delay(UnbrokenTrials),...
-%       PredictedMeans,...
-%       nans(sum(UnbrokenTrials),1),...
-%       mean_visible,...
       ]);   
   if ~isempty(data.spikes.data)
   spikies=[spikies;data.spikes.data(UnbrokenTrials,find(imptUnits==1))];
@@ -282,41 +223,17 @@ Interm((allData(:,1)==i),1)={theFile};
 end
 % Make a Table
 
-
-%  1. session
-%  2. Trial index within session
-%  3. Hazard rate
-%  4. Noise
-%  5. TACP(Actual)
-%  6. T1_Angle
-%  7. T2_Angle
-%  8. Sample Angle
-%  9. LLR for T1>T2
-%  10. Offline Score: 0=err, 1=correct, 2=sample, -1=ncerr
-%  11. Choices on when made response? 0=no, 1=yes
-%  12. Response_angle
-%  13. Active_Angle 
-%  14. Choice
-%  15. Choice~=Correct(previous trial) aka switch
-%  16. LLR for Change point
-%  17. OnlineScore: 
-
 thenames={'Session','Trial_Within_Session','H_Rate',...
                                              'Noise', 'TACP_Actual', 'T1_Angle',...
                                              'T2_Angle', 'Sample_Angle', 'LLR', 'Offline_Score', 'Correct_On',...
                                              'Response_Angle', 'Active_Angle', 'Choice',...
-                                             'Switch', 'LLR_for_Change', 'OnlineScore','Correct','SampleOn','SampleOff','FPOff','SacOn','Num_Neuron'};
+                                             'Switch', 'LLR_for_Change', 'OnlineScore','Correct','SampleOn','SampleOff','FPOff','SacOn','TargAcq','Num_Neuron'};
 thenames=[thenames,spikelist];
 
 allData=cell2table(Interm,'VariableNames',thenames);
 else
     allData=[];
 end
-
-%%                                         
-
-
-
 
 end
 
